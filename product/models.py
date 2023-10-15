44274899a5c1253e793d3dd3e42f54e5bc3970a5
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-10-15 17:42:13 +0600

    product model create

---
+++ product/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
